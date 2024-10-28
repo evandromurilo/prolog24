1a3a02af497346c3aaafb6615dc5906bf05c3505
murilo <evandromurilob@hotmail.com>
2024-10-28 07:00:02 -0400

    add inventory_report to customer

--- customer.pl
+++ customer.pl
@@ -1,5 +1,5 @@
 customer(tomas, boston, aaa).
-customar(julio, nyc, bbb).
+customer(julio, nyc, bbb).
 
 item(1, coke, 10).
 item(2, rice, 5).
@@ -7,4 +7,15 @@ item(2, rice, 5).
 inventory(1, 30).
 inventory(2, 14).
 
+item_quantity(Name, Qtd) :-
+    item(Num, Name, _),
+    inventory(Num, Qtd).
 
+inventory_report() :-
+    item_quantity(Name, Qtd),
+    write(Name), tab(2), write(Qtd), nl,
+    fail.
+
+inventory_report().
+
+good_customer(C) :- customer(C, _, aaa).
