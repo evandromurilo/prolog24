5390b8e582e4a875dc972e876f318348df1d7024
murilo <evandromurilob@hotmail.com>
2024-10-30 17:09:24 -0400

    add reorder and valid_order to customer.pl

--- customer.pl
+++ customer.pl
@@ -5,7 +5,7 @@ item(1, coke, 10).
 item(2, rice, 5).
 
 inventory(1, 30).
-inventory(2, 14).
+inventory(2, 2).
 
 item_quantity(Name, Qtd) :-
     item(Num, Name, _),
@@ -19,3 +19,14 @@ inventory_report() :-
 inventory_report().
 
 good_customer(C) :- customer(C, _, aaa).
+
+reorder(Item) :-
+    item(Code, Item, Reorder),
+    inventory(Code, InStock),
+    Reorder >= InStock,
+    write('Time to reorder').
+
+valid_order(Customer, Item, Quantity) :-
+    good_customer(Customer),
+    item_quantity(Item, InStock),
+    Quantity =< InStock.
