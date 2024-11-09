e5edee80ef9353fb7c1724aff2681756d3935a16
murilo <evandromurilob@hotmail.com>
2024-11-09 07:08:15 -0400

    add known cache to house

--- house.pl
+++ house.pl
@@ -21,8 +21,39 @@ animal(cachorro).
 tamanho(X) :- ask(tamanho, X).
 come(X) :- ask(come, X).
 
+:- dynamic known/3.
+
 ask(Attr, Val) :-
+    known(Attr, Val, sim).
+ask(Attr, Val) :-
+    known(Attr, Val, nao),
+    fail.
+ask(Attr, Val) :-
+    not(known(Attr, Val, _X)),
     write(Attr),tab(1),write(Val),
     tab(1),write('(sim/nao)'),write(?),tab(1),
     read(X),
+    asserta(known(Attr, Val, X)),
     X = sim.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
